b5358427a422716e89de20959c0c5a21eeec15c9
Mikhail Filatov <misha@inspirational.ru>
2019-01-07 00:13:00 +0300

    Init

---
+++ src/main/java/ru/mfilatov/SmallTests/Runner.java
@@ -0,0 +1,10 @@
+package ru.mfilatov.SmallTests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runner {
+    private static Logger logger = LoggerFactory.getLogger(Runner.class);
+    public static void main(String[] args) {
+    }
+}
