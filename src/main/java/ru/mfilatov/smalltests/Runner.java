0ebfdf1ccf873d02f040b4fe36e728650ac1c41a
Mikhail Filatov <map7000@gmail.com>
2023-08-13 18:34:19 +0000

    fix package name

---
+++ src/main/java/ru/mfilatov/smalltests/Runner.java
@@ -0,0 +1,10 @@
+package ru.mfilatov.smalltests;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Runner {
+  public static void main(String[] args) {
+    log.info("It's just a test");
+  }
+}
--- src/test/java/XorTest.java
+++ src/test/java/XorTest.java
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class XorTest {
-    public static boolean xor(boolean a, boolean b){
-        return a ^ b;
-    }
+  public static boolean xor(boolean a, boolean b) {
+    return a ^ b;
+  }
 
-    @Test
-    @DisplayName("True XOR True")
-    void trueXORtrue(){
-        Assertions.assertFalse(xor(true, true));
-    }
+  @Test
+  @DisplayName("True XOR True")
+  void trueXORtrue() {
+    Assertions.assertFalse(xor(true, true));
+  }
 
-    @Test
-    @DisplayName("True XOR False")
-    void trueXORfalse(){
-        Assertions.assertTrue(xor(true, false));
-    }
+  @Test
+  @DisplayName("True XOR False")
+  void trueXORfalse() {
+    Assertions.assertTrue(xor(true, false));
+  }
 
-    @Test
-    @DisplayName("False XOR True")
-    void falseXORtrue(){
-        Assertions.assertTrue(xor(false, true));
-    }
+  @Test
+  @DisplayName("False XOR True")
+  void falseXORtrue() {
+    Assertions.assertTrue(xor(false, true));
+  }
 
-    @Test
-    @DisplayName("False XOR False")
-    void simple(){
-        Assertions.assertFalse(xor(false, false));
-    }
+  @Test
+  @DisplayName("False XOR False")
+  void simple() {
+    Assertions.assertFalse(xor(false, false));
+  }
 }
